bb68432f43d609987b9de874f58c38c2100e623d
Ana María <misushima@gmail.com>
2021-05-27 21:35:39 -0400

    Se agrega event listener para el boton de submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,13 @@ class Mascota extends Animal {
   }
 } 
 
+document.addEventListener('click', function(event) {
+  // If the clicked element doesn't have the right selector, bail
+  if (!event.target.matches('form button')) return;
+
+  // Don't follow the link
+	event.preventDefault();
+
+	// Log the clicked element in the console
+	console.log('target:', event.target);
+}, false);
