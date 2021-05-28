4bd17318407fa0cfe441070bf25d23ea2b233110
Ana María <misushima@gmail.com>
2021-05-27 21:32:34 -0400

    Se agrega clase mascota según diagrama

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,23 @@ class Animal extends Propietario {
   }
 }
 
+class Mascota extends Animal {
+  constructor(nombreDueno, direccion, telefono, tipo, nombreMascota, enfermedad) {
+    super(nombreDueno, direccion, telefono, tipo);
+    this._nombreMascota = nombreMascota;
+    this._enfermedad = enfermedad;
+  }
+  get nombreMascota() {
+    return this._nombreMascota;
+  }
+  set nombreMascota(newNombre) {
+    this._nombreMascota = newNombre;
+  }
+  get enfermedad() {
+    return this._enfermedad;
+  }
+  set enfermedad(newEnfermedad) {
+    this._enfermedad = newEnfermedad;
+  }
+} 
+
